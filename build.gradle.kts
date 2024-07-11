8418223442f10fd30fa1067cf64a246cbfae91f6
Jun Kurihara <lh182051+src@gmail.com>
2024-07-11 12:32:16 +0900

    Initial commit
    This commit has the feature of simple chat communication.

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.5"
+//    id("org.graalvm.buildtools.native") version "0.10.2"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
+}
+
+group = "io.github.kuju63"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+extra["springAiVersion"] = "1.0.0-M1"
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.ai:spring-ai-ollama-spring-boot-starter")
+    // Documentに記載はないがspring-ai-ollama-spring-boot-starterが依存している
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    // Documentに記載はないがspring-ai-ollama-spring-boot-starterが依存している
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("io.projectreactor:reactor-test")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/io/github/kuju63/spring/ai/controller/ChatController.kt
@@ -0,0 +1,31 @@
+package io.github.kuju63.spring.ai.controller
+
+import org.springframework.ai.chat.messages.UserMessage
+import org.springframework.ai.chat.model.ChatModel
+import org.springframework.ai.chat.model.ChatResponse
+import org.springframework.ai.chat.prompt.Prompt
+import org.springframework.ai.ollama.api.OllamaOptions
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+
+@RestController
+class ChatController(private val chatModel: ChatModel) {
+    @GetMapping("/ai/generate")
+    fun generate(
+        @RequestParam(value = "message", defaultValue = "Tell me a joke") message: String): ChatResponse {
+        val options = OllamaOptions()
+        options.model = "llama3"
+        val userMessage = UserMessage(message)
+        val prompt = Prompt(userMessage, options)
+        return chatModel.call(prompt)
+    }
+
+    @GetMapping("/ai/generateStream")
+    fun generateStream(
+        @RequestParam(value = "message", defaultValue = "Tell me a joke") message: String): Flux<ChatResponse> {
+        val prompt = Prompt(message)
+        return chatModel.stream(prompt)
+    }
+}
